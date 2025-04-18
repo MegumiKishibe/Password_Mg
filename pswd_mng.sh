1ac07817dba703e545daf19598b5fc014e2d86cd
Megumi Kishibe <Kishibe0417@gmail.com>
2025-04-18 12:37:47 +0900

    とりあえず！

---
+++ pswd_mng.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+        echo "パスワードマネージャーへようこそ！"
+        
+        echo " Enter your name:"
+                read name       > service_name.txt
+        echo " Enter your username:"
+                read username   > user_name.txt
+        echo " Enter your password:"
+                read password   >pass_word.txt
+                
+        echo "Thank you!"
+        
+
