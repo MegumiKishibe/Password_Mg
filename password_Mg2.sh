d5473178d9cdbf6e24c82b187a0c012922e9f163
Megumi Kishibe <Kishibe0417@gmail.com>
2025-04-22 18:22:22 +0900

    一時保存

--- password_Mg2.sh
+++ password_Mg2.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-	echo "パスワードマネージャーへようこそ！"
-	while true
-	do
-	echo "次の選択肢から入力してください(Add Password/Get Password/Exit)"
-	read personal_info
+        echo "パスワードマネージャーへようこそ！"
+        while true; do
+        echo "次の選択肢から入力してください(Add Password/Get Password/Exit)"
+        read personal_info
 
-		if [ "$personal_info" == "Add Password" ]; then 
-			echo "サービス名を入力してください："
-			read sevicename
-			echo "$service_name" >> service_name.txt
- 
-			echo "ユーザー名を入力してください："
-			read username
-			echo "$username" >> user_name.txt
+                if [ "$personal_info" == "Add Password" ]; then
+                        echo "サービス名を入力してください："
+                        read service_name
+                        echo "$service_name" >> service_name.txt
 
-			echo "パスワードを入力してください："
-			read password
-			echo "$password" >> pass_word.txt
+                        echo "ユーザー名を入力してください："
+                        read username
+                        echo "$username" >> user_name.txt
 
-			echo "パスワードの追加は成功しました。
+                        echo "パスワードを入力してください："
+                        read password
+                        echo "$password" >> pass_word.txt
 
-		elif [ "$personal_info" == "Get Password"]; then
-			echo "サービス名を入力してください："
-			read service_name
+                        echo "パスワードの追加は成功しました。"
 
-			find ./ -type f -name 'service_name.txt' | xargs grep -n "${service_name}"
-		
-			if [ "${servicce_name}" !=  ]
-			echo "そのサービス名は登録されていません。"
+                elif [ "$personal_info" == "Get Password" ]; then
+                        echo "サービス名を入力してください："
+                        read service_name
 
-			elif [ service_name == "${service_name}" ] 
-			echo "サービス名：./servise_name.txt"
-			echo "ユーザー名：./user_name.txt"
-			echo "パスワード：./pass_word.txt"
-			fi
-        	
-		elif [ "$personal_info" == "Exit" ]; then
-			echo "Thank you!"
-			exit 0
+                        line=$(grep -n "^${service_name}$" service_name.txt | cut -d: -f1) # *
 
-		else 
-			echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
-		fi
-	done
+                        if [ -z "$line" ]; then #**
+                                echo "そのサービス名は登録されていません。"
+
+                        else
+                                echo "サービス名：$(sed -n "${line}p" service_name.txt)" # ***
+                                echo "ユーザー名：$(sed -n "${line}p" user_name.txt)"
+                                echo "パスワード：$(sed -n "${line}p" pass_word.txt)"
+                        fi
+
+                elif [ "$personal_info" == "Exit" ]; then
+                        echo "Thank you!"
+                        exit 0
+
+                else
+                        echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+                fi
+        done
+
